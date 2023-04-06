e76b438b56c6b3dd6338f57c81868babba81bf59
tarar7 <23100225@lums.edu.pk>
2023-04-06 17:15:06 +0500

    Automation test case login

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -2,5 +2,10 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit(Cypress.env('base_url'))
     cy.wait(2000)
+    cy.on('uncaught:exception', (err, runnable) => {
+      console.log("Error", err)
+      return false
+    })
+    cy.contains('Featured Products').should('exist')
   })
 })
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,44 @@
 describe('Login Functionality', () => {
-    it('Logs in successfully', () => {
-      cy.visit('/login')
-      cy.get('#email').type('user@example.com')
-      cy.get('#password').type('password')
-      cy.get('#login-button').click()
-      cy.url().should('include', '/dashboard')
-    })
+    it('Customer logs in successfully', () => {
+      cy.visit(Cypress.env('base_url') + '/login')
+      cy.wait(2000)
+      cy.on('uncaught:exception', (err, runnable) => {
+        console.log("Error", err)
+        return false
+      })
+      cy.get('input[data-cy=email-login]').type('arslantarar@gmail.com');
+      cy.get('input[data-cy=password-login]').type('abcd1234');
+      cy.get('input[data-cy=customer-radio]').check();
+      cy.get('button[data-cy=login-button]').click();
+      cy.url().should('eq', 'http://localhost:3000/');
+      cy.wait(3000)
+    });
+
+    it('Admin logs in successfully', () => {
+      cy.visit(Cypress.env('base_url') + '/login')
+      cy.wait(2000)
+      cy.on('uncaught:exception', (err, runnable) => {
+        console.log("Error", err)
+        return false
+      })
+      cy.get('input[data-cy=email-login]').type('nafeymoiz@gmail.com');
+      cy.get('input[data-cy=password-login]').type('aabbccdd');
+      cy.get('input[data-cy=admin-radio]').check();
+      cy.get('button[data-cy=login-button]').click();
+      cy.url().should('eq', 'http://localhost:3000/Admin');
+    });
+
+    it('Displays an error message for invalid credentials', () => {
+      cy.visit(Cypress.env('base_url') + '/login')
+      cy.wait(2000)
+      cy.on('uncaught:exception', (err, runnable) => {
+        console.log("Error", err)
+        return false
+      })
+      cy.get('input[data-cy=email-login]').type('invalid@gmail.com')
+      cy.get('input[data-cy=password-login]').type('password')
+      cy.get('input[data-cy=customer-radio]').check()
+      cy.get('button[data-cy=login-button]').click()
+      cy.contains('Incorrect Email or Password. Please try again.').should('exist')
+    });
   })
