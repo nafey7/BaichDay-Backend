17caab071c0fcf6324ef3cc606d16186f4276d69
Mahad Mubashir <94633830+mahadmubashir@users.noreply.github.com>
2023-04-06 23:22:06 +0500

    wallet test

--- cypress/e2e/wallet.cy.js
+++ cypress/e2e/wallet.cy.js
@@ -7,12 +7,35 @@ describe('Add Balance Functionality', () => {
             console.log("Error", err)
             return false
         })
-        cy.get('input[data-cy=email-login]').type('arslantarar@gmail.com');
+        cy.get('input[data-cy=email-login]').type('waqarzaka@gmail.com');
         cy.get('input[data-cy=password-login]').type('abcd1234');
         cy.get('input[data-cy=customer-radio]').check();
         cy.get('button[data-cy=login-button]').click();
-        cy.wait(2000)
-        
+        cy.get('#profile-icon').click()
+        cy.get('#wallet-id').click()
+        cy.url().should('eq', 'http://localhost:3000/wallet')
+        cy.wait(3000);
+        cy.get('#current-balance')
+        .invoke('text')
+        .then(originalValue => {
+            console.log('Original balance:', originalValue);
+
+            // Add balance to the wallet
+            cy.get('button[data-cy=add-balance-button]').click();
+            cy.url().should('eq', 'http://localhost:3000/payment')
+            cy.get('input[data-cy=balance-amount]').clear().type(2000);
+            cy.get('button[data-cy=confirm-payment-button]').click();
+            cy.url().should('eq', 'http://localhost:3000/wallet')
+            cy.wait(3000);
+
+            // Get the updated balance value and compare to original value
+            cy.get('#current-balance')
+                .invoke('text')
+                .then(newValue => {
+                    console.log('New balance:', newValue);
+                    expect(parseInt(newValue.substring(1,))).to.equal(parseInt(originalValue.substring(1,))+2000);
+                });
+        });
 
       })
 })
